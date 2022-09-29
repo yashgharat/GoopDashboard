2b376beac5e0272f524f33b3c6c6e8a2874f67ac
Yash Gharat <yash.gharat@knights.ucf.edu>
2022-09-29 15:24:29 -0400

    canvas api

--- test_files/canvas-test.py
+++ test_files/canvas-test.py
@@ -1,17 +1,21 @@
+from statistics import mean
 from canvasapi import Canvas
 
 URL = 'https://webcourses.ucf.edu/'
-TOKEN = '1158~pFba77zAczsbMfE3ro4Rke2iBeiGGG4VcyqwRRD68jjO0ux45KCPVp5GWON9qsK6'
+TOKEN = '1158~o2rwRtutzEvgcVSTkUlwJqe7FFGqgkqCAF3QA4aVWrBtrq8qgiKRQc2hlPEksj0E'
 
 canvas = Canvas(URL, TOKEN)
 
 me = canvas.get_current_user()
 
-# # my_enrollments = me.get_enrollments(type=["StudentEnrollment"], state=["active"])
-# my_courses = me.get_courses()
+my_courses = me.get_favorite_courses()
 
-# for course in my_courses:
-#     # The enrollments API only provides the course ID. For more info about the course,
-#     # you'll need to call canvas.get_course() and pass in the ID
-#     print(vars(course)['enrollments'])
-print
+for course in my_courses:
+    # The enrollments API only provides the course ID. For more info about the course,
+    # you'll need to call canvas.get_course() and pass in the ID
+    # print(vars(course)['enrollments'])
+    assignments = course.get_assignments()
+
+    for assignment in assignments:
+        submission = assignment.get_submission('4183842')
+        print(vars(assignment)['has_submitted_submissions'])
